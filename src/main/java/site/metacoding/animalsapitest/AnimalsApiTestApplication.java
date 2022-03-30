6eb2d0f947324f022797ef3fb4a21fd8beac2b16
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-03-30 16:00:05 +0900

    API 테스트 시작

---
+++ src/main/java/site/metacoding/animalsapitest/AnimalsApiTestApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.animalsapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsApiTestApplication.class, args);
+	}
+
+}
